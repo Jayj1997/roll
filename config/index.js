ddda9ee93804104da7e40aef3ea1e0ff8c88d162
jayj1997 <1055358033@qq.com>
2020-09-16 22:13:43 +0800

    线上线下有点区别啊

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://127.0.0.1:9000',
+        target: 'http://localhost:9000',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '/api'
